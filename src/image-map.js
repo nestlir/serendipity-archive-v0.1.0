d4ad5b9d5b59f76a606ced3c12653b56855084dc
Nestlir <100813649+nestlir@users.noreply.github.com>
2026-08-22 14:17:03 +0300

    fix: use semantic image references for archive entries

--- src/image-map.js
+++ src/image-map.js
@@ -1,22 +1,25 @@
-/* Curated prototype references. Keep status=review until each asset is downloaded, optimized and rights metadata is verified. */
+/* Prototype editorial image references. Verify final license/credit before publication. */
+const commons = (file) => `https://commons.wikimedia.org/wiki/Special:Redirect/file/${encodeURIComponent(file)}`;
+
 export const imageMap = {
-  'KŌDŌ':'https://commons.wikimedia.org/wiki/Special:Redirect/file/JapaneseIncenseBurner_KouroOnTokonoma.jpg',
-  'TEA BOWL':'https://commons.wikimedia.org/wiki/Special:Redirect/file/Japanese_-_Tea_Bowl_-_Walters_49233.jpg',
-  'KYŪSU':'https://commons.wikimedia.org/wiki/Special:Redirect/file/Tea_bowl%2C_known_as_Suehiro%2C_studio_of_Chojiro%2C_Raku_ware%2C_Kuroraku_type%2C_Azuchi-Momoyama_to_Edo_period%2C_1500s-1600s_AD%2C_ceramic_-_Tokyo_National_Museum_-_Ueno_Park%2C_Tokyo%2C_Japan_-_DSC08889.jpg',
-  'FUSHIMI INARI':'https://commons.wikimedia.org/wiki/Special:Redirect/file/20181110_Fushimi_Inari_Torii_1.jpg',
-  'SAGANO':'https://commons.wikimedia.org/wiki/Special:Redirect/file/20181110_Fushimi_Inari_Torii_11.jpg',
-  'KYOTO':'https://commons.wikimedia.org/wiki/Special:Redirect/file/Kyoto_Fushimi_Inari-taisha_Eingangs-Torii.jpg',
-  'SUWON':'https://commons.wikimedia.org/wiki/Special:Redirect/file/Suwon%2C_Hwaseong_Fortress.jpg',
-  'WASHI':'https://commons.wikimedia.org/wiki/Special:Redirect/file/Shiroishi_washi_letter_paper.jpg',
-  'KINTSUGI':'https://commons.wikimedia.org/wiki/Special:Redirect/file/Kintugi.jpg',
-  'SEAL SCRIPT':'https://commons.wikimedia.org/wiki/Special:Redirect/file/%E6%95%95-seal.svg'
+  'KŌDŌ': commons('Japanese - Incense Burner ("Koro") - Walters 49466.jpg'),
+  'TEA BOWL': commons('Japanese_-_Tea_Bowl_-_Walters_49233.jpg'),
+  'KYŪSU': commons('JapaneseTeapot.jpg'),
+  'FUSHIMI INARI': commons('20181110_Fushimi_Inari_Torii_1.jpg'),
+  'SAGANO': commons('20181110_Fushimi_Inari_Torii_11.jpg'),
+  'KYOTO': commons('Kyoto_Fushimi_Inari-taisha_Eingangs-Torii.jpg'),
+  'SUWON': commons('Suwon_Hwaseong_Fortress.jpg'),
+  'WASHI': commons('Shiroishi_washi_letter_paper.jpg'),
+  'KINTSUGI': commons('Kintsugi.jpg'),
+  'SEAL SCRIPT': commons('Seal_script_calligraphy.jpg')
 };
 
 export const gallerySets = {
+  'KŌDŌ':[imageMap['KŌDŌ'],commons('Japanese - Incense Burner ("Koro") in the Form of the Kirin - Walters 491731 - Three Quarter.jpg'),commons('JapaneseIncenseBurner KouroOnTokonoma.jpg')],
   'TEA BOWL':[imageMap['TEA BOWL'],imageMap['KINTSUGI'],imageMap['KYŪSU']],
-  'KINTSUGI':[imageMap['KINTSUGI'],imageMap['TEA BOWL'],imageMap['WASHI']],
+  'KYŪSU':[imageMap['KYŪSU'],commons('Kyusu by i yudai in Ueno, Tokyo.jpg'),commons('Household-kyusu-feb5-2015.jpg')],
   'FUSHIMI INARI':[imageMap['FUSHIMI INARI'],imageMap['KYOTO'],imageMap['SAGANO']],
   'KYOTO':[imageMap['KYOTO'],imageMap['FUSHIMI INARI'],imageMap['SAGANO']],
   'WASHI':[imageMap['WASHI'],imageMap['KINTSUGI'],imageMap['TEA BOWL']],
-  'KŌDŌ':[imageMap['KŌDŌ'],imageMap['TEA BOWL'],imageMap['KINTSUGI']]
+  'KINTSUGI':[imageMap['KINTSUGI'],imageMap['TEA BOWL'],imageMap['WASHI']]
 };
